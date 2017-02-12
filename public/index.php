ac6f77a18a13f080a823bed5b365d858405ba17b
Marcos Barbosa <barbosa.mg@gmail.com>
2017-02-11 22:40:52 -0200

    Exemplo de criação de diversos tipos de rotas

--- public/index.php
+++ public/index.php
@@ -8,4 +8,18 @@ $app->get('/', function() {
 	return "Minha primeira aplicação com silex.";
 });
 
+$app->get('/home', function() {
+	return "
+			<form method='post' action='/home'>
+				<input type='text' name='name'>
+				<button type='submit'>Enviar</button>
+			</form>";
+});
+
+
+$app->post('/home', function() {
+	return "Post foi enviado";
+});
+	
+
 $app->run();
